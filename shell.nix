e0eb0b3975c321c8105985e0ac7bad62c7922ca6
Erik Härkönen <erik.harkonen@hotmail.com>
2026-05-29 11:41:19 +0300

    MacOS works

--- shell.nix
+++ shell.nix
@@ -1,5 +1,5 @@
-#{ pkgs ? import <nixpkgs> { config = { allowUnfree = true; cudaSupport = true; }; } }:
-{ pkgs ? import <nixpkgs> {} }:
+{ pkgs ? import <nixpkgs> { config = { allowUnfree = true; cudaSupport = true; }; } }:
+#{ pkgs ? import <nixpkgs> {} }:
 
 pkgs.mkShell {
   packages = with pkgs; [
@@ -8,6 +8,7 @@ pkgs.mkShell {
     pkg-config
     gcc
     glfw
+    git  # MacOS built-in too old
     vulkan-loader
     vulkan-headers
     xorg.libX11
@@ -19,6 +20,11 @@ pkgs.mkShell {
     #cudatoolkit
     #cudaPackages.cuda_nvrtc
     pkgs.bashInteractive # direnv shopt errors
+  ] ++ pkgs.lib.optionals pkgs.stdenv.hostPlatform.isDarwin [
+    #pkgs.apple-sdk_15
+    pkgs.darwin.xcode_16_3
+    #pkgs.darwin.developer_cmds
+    (pkgs.darwinMinVersionHook "15.0")
   ];
 
   LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath (with pkgs; [
@@ -36,6 +42,10 @@ pkgs.mkShell {
   ]);
 
   shellHook = ''
+    ${pkgs.lib.optionalString pkgs.stdenv.hostPlatform.isDarwin ''
+      export DEVELOPER_DIR=${pkgs.darwin.xcode_16_3}/Contents/Developer/
+    ''}
+
     echo "Build and run with:"
     echo "cmake --preset default -DSLANG_RHI_BUILD_EXAMPLES=ON -DSLANG_RHI_FETCH_SLANG=ON"
     echo "cmake --build -DSLANG_RHI_DEBUG=1 --preset Release --target example-path-tracer"
